3be889745235255bd6536394fd8c1f9fcc4e980c
Vamshi Reddy <vamshiproject02@gmail.com>
2024-02-22 22:28:39 +0530

    working on the assigning part
    Signed-off-by: Vamshi Reddy <vamshiproject02@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,25 @@ import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-  
-    const token = core.getInput("github-token");
-    const command = core.getInput("prow-command", { required: true })
+    const token = core.getInput('github-token')
+    const command = core.getInput('prow-command', { required: true })
 
     core.debug(JSON.stringify(github.context.payload))
 
     const issueNumber: number | undefined = github.context.payload.issue?.number
-    const commentBody: string = github.context.payload["comment"]["body"]
-    const commenterId: string = github.context.payload["comment"]["user"]["login"]
+    const commentBody: string = github.context.payload['comment']['body']
+    const commenterId: string =
+      github.context.payload['comment']['user']['login']
 
     if (commentBody.includes(command)) {
       const octokit = new github.GitHub(token)
 
       await octokit.issues.addAssignees({
         ...github.context.repo!,
-        'issue_number': issueNumber!,
-        'assignees': [commenterId]
+        issue_number: issueNumber!,
+        assignees: [commenterId]
       })
     }
-
   } catch (error: any) {
     core.setFailed(error.message)
   }
